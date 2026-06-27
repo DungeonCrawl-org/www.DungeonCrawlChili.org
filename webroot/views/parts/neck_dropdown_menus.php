6bc695ee754a274439bfacc37712a5a24aa9cc00
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-26 21:36:44 -0400

    Revise dropdown menu links and donation text
    Updated links and text in the neck dropdown menus.

--- webroot/views/parts/neck_dropdown_menus.php
+++ webroot/views/parts/neck_dropdown_menus.php
@@ -1,12 +1,9 @@
 	<div class="dropdown" style="float:left;">
 		<button class="dropbtn">Menu</button>
 		<div class="dropdown-content" style="left:0;">
-			<a href="/">Dungeon Crawl Forks homepage</a>
-			<a href="https://discord.gg/gMnE5JFcB7" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl community discord server</a>
-			<a href="/online_servers.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Play online on a Dungeon Crawl server</a>
-			<a href="/download_forks.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download a Dungeon Crawl fork for offline play</a>
-			<a href="/forks_details.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; View Dungeon Crawl forks details</a>
-			<a href="https://www.reddit.com/r/dungeoncrawl/" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl subreddit</a>
+			<a href="https://discord.gg/gMnE5JFcB7" target="_blank">Dungeon Crawl community discord server</a>
+			<a href="https://www.dungeoncrawlforks.org/" target="_blank">Dungeon Crawl Forks homepage</a>
+			<a href="https://www.reddit.com/r/dungeoncrawl/" target="_blank">Dungeon Crawl subreddit</a>
 			<a href="https://www.crawlcosplay.org/" target="_blank">Crawl Cosplay (CC) website</a>
 			<a href="https://discord.gg/pW7nqC8Wu3" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Crawl Cosplay 2.0 community discord server</a>
 		</div>
@@ -15,7 +12,7 @@
 	<div class="dropdown" style="float:right;">
 		<button class="dropbtn">Donate!</button>
 		<div class="dropdown-content" style="right:0;">
-			Please donate for the cost of our CCO.CrawlCosplay.org WebTiles server at:<br>
+			Please donate for the hosting cost of our Dungeon Crawl and Crawl Cosplay websites:<br>
 			<a href="https://patreon.com/rogga" target="_blank"><b style="color:yellow">Patreon</b></a>
 		</div>
 	</div>
