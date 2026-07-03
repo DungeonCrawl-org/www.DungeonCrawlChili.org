81cca039ae44916ca84018c5d0688f2871c0df70
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-03 08:34:20 -0400

    Update changesInDCChili.php with new features

--- webroot/changesInDCChili.php
+++ webroot/changesInDCChili.php
@@ -1,11 +1,22 @@
-Implemented already
-- the species Silent Specter was added from BCadren crawl.,
-- D:1 is mostly a forested open floor layout.,
-- D:2 is always the same static floor (for now until someone makes a few new full floor vaults).,
-- Jessica right now always spawns in the small castle on the D:2 static floor layout, and drops a book 50% of time.,
-- D:3 has 3 vaults of castles. this still needs testing since I keep getting the same castle when testing on DCF.,
-- There's a new late game Unique called Regal (an octopode with a cloak) that was never added to DCSS over a year ago and was added to DC Chili by @MrSquiggles . (I don't know who the original author was though.),
+<?php require 'views/header.php'; ?> 
+<?php require 'views/neck.php';   ?>
+<h1>Changes to Dungeon Crawl Chili</h1>
+<h2>Implemented already:</h2>
+<ul><li>The species Silent Specter was added from BCadren crawl;</li>
+    <li>D:1 is mostly a forested open floor layout;</li>
+    <li>D:2 is always the same static floor (for now until someone makes a few new full floor vaults);</li>
+    <li>Jessica right now always spawns in the small castle on the D:2 static floor layout, and drops a book 50% of time;</li>
+    <li>D:3 has 3 vaults of castles. this still needs testing since I keep getting the same castle when testing on DCF;</li>
+    <li>There's a new late game Unique called Regal (an octopode with a cloak) that was never added to DCSS over a year ago and was added to DC Chili by @MrSquiggles . (I don't know who the original author was though.)</li>
+</ul>
 
-To Come:
-- D:4 will be @bcadren 's Sewer level...but might be D:3 instead.
-- Medusa, a naga with the Petrify spell, that will always show up on D:2. The intent will be to let her be on D:2 until the player has enough experience...sort of like an early Sygmund.
+<h2>Already planned:</h2>
+<ul><li>D:4 will be @bcadren 's Sewer level...but might be D:3 instead;</li>
+    <li>Medusa, a naga with the Petrify spell, that will always show up on D:2. The intent will be to let her be on D:2 until the player has enough experience...sort of like an early Sygmund.</li>
+    <li>Shorten the number of floors for each branch (including Dungeon) by 1 floor (except for Orcish Mines)</li>
+    <li>To compensate for the loss of XP from 1 less floor in branches, make Uniques much more frequent.</li>
+</ul>
+
+<h2>Suggestions to be considered:</h2>
+<p>none yet</p>
+<?php require 'views/footer.php'; ?>
