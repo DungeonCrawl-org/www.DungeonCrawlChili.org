4c7a383d908fd9112695f5fb9f49e35f92d8b6c1
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-03 08:23:39 -0400

    Add changes and features for DC Chili
    Documented changes and new features in DC Chili, including new species, layouts, and character spawns.

---
+++ webroot/changesInDCChili.php
@@ -0,0 +1,11 @@
+Implemented already
+- the species Silent Specter was added from BCadren crawl.,
+- D:1 is mostly a forested open floor layout.,
+- D:2 is always the same static floor (for now until someone makes a few new full floor vaults).,
+- Jessica right now always spawns in the small castle on the D:2 static floor layout, and drops a book 50% of time.,
+- D:3 has 3 vaults of castles. this still needs testing since I keep getting the same castle when testing on DCF.,
+- There's a new late game Unique called Regal (an octopode with a cloak) that was never added to DCSS over a year ago and was added to DC Chili by @MrSquiggles . (I don't know who the original author was though.),
+
+To Come:
+- D:4 will be @bcadren 's Sewer level...but might be D:3 instead.
+- Medusa, a naga with the Petrify spell, that will always show up on D:2. The intent will be to let her be on D:2 until the player has enough experience...sort of like an early Sygmund.
