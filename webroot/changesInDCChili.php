d4df0434a2d1f89a6cc56ce9063884872a61fa7a
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-03 11:46:22 -0400

    Refine descriptions of new game elements in changesInDCChili.php

--- webroot/changesInDCChili.php
+++ webroot/changesInDCChili.php
@@ -9,14 +9,15 @@
     <li>D:2 is always the same static floor (for now until someone makes a few new full floor vaults);</li>
     <li>Jessica right now always spawns in the small castle on the D:2 static floor layout, and drops a book 50% of time;</li>
     <li>D:3 has 3 vaults of castles. this still needs testing since I keep getting the same castle when testing on DCF;</li>
-    <li>There's a new late game Unique called Regal (an octopode with a cloak) that was never added to DCSS over a year ago and was added to DC Chili by @MrSquiggles . (I don't know who the original author was though.)</li>
+    <li>There's a new late game Unique called Regal (an octopode with a cloak) that was never added to DCSS over a year ago and was added to DC Chili by MrSquiggles. (I don't know who the original author was though.)</li>
 </ul>
 
 <h2>Already planned:</h2>
 <ul><li>D:4 will be @bcadren 's Sewer level;</li>
-    <li>Medusa, a naga with the Petrify spell, that will always show up on D:2. The intent will be to let her be on D:2 until the player has enough experience...sort of like an early Sygmund.</li>
+    <li>Medusa, a naga Unique with the Petrify spell, will always show up on D:2. The intent will be to let her be on D:2 until the player has enough experience...sort of like an early Sygmund.</li>
     <li>Shorten the number of floors for each branch (including Dungeon) by 1 floor (except for Orcish Mines)</li>
     <li>To compensate for the loss of XP from 1 less floor in most branches and Dungeon, we'll make Uniques much more frequent to spawn and possibly worth a bit more XL.</li>
+    <li>Make Jessica not always give Blink as a Spell, so that only 25% of times you get the change of getting Blink on kill...as opposed to 50% of the time</li>
 </ul>
 
 <h2>Suggestions to be considered:</h2>
