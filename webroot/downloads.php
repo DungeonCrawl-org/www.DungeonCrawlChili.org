d797fe5a1eb1f7e84dcfed5ab68ac20b81cc7363
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-04 18:33:36 -0400

    Update download information formatting
    Separated the download information into two paragraphs for clarity.

--- webroot/downloads.php
+++ webroot/downloads.php
@@ -1,7 +1,8 @@
 <?php require 'views/header.php'; ?> 
 <?php require 'views/neck.php';   ?>
 <h1 style="color:orange">Download page for Dungeon Crawl Chili</h1>
-<p>Downloads are available on demand for Tiles or Console and for other OSes when not listed below. Please ask RoGGa in the DC discord linked above.</p>
+<p>Downloads are available on demand for Tiles or Console and for other OSes when not listed below.</p>
+<p>Please ask RoGGa in the DC discord linked above.</p>
 <h2>Linux builds</h2>
 <h3>Linux Tiles builds</h2>
 <ul><li>2026-07-04: <a href="/downloads/DungeonCrawlChili-1.0-a0-72-g0837f3004f-tiles-linux-x86_64.tar.xz">DungeonCrawlChili-1.0-a0-72-g0837f3004f-tiles-linux-x86_64.tar.xz</a></li></ul>
