e49f29232069c5c5973e1ea9c62924f67a7ccf1a
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-04 16:35:47 -0400

    Add downloads.php for Dungeon Crawl Chili

---
+++ webroot/downloads.php
@@ -0,0 +1,8 @@
+<?php require 'views/header.php'; ?> 
+<?php require 'views/neck.php';   ?>
+<h1 style="color:orange">Download page for Dungeon Crawl Chili</h1>
+<h2>Linux builds</h2>
+<h3>Linux Tiles builds</h2>
+<ul><li>2026-07-04: <a href="/downloads/DungeonCrawlChili-1.0-a0-72-g0837f3004f-tiles-linux-x86_64.tar.xz">DungeonCrawlChili-1.0-a0-72-g0837f3004f-tiles-linux-x86_64.tar.xz</a></li></ul>
+
+<?php require 'views/footer.php'; ?>
