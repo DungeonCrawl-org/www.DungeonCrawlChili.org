013d105ed3296b30ee45e02b21a4d0336a99965a
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-16 12:03:23 -0400

    Revise changes in Dungeon Crawl Chili
    Updated the changes log for Dungeon Crawl Chili with new features and adjustments.

--- webroot/changesInDCChili.php
+++ webroot/changesInDCChili.php
@@ -1,23 +1,26 @@
 <?php require 'views/header.php'; ?> 
 <?php require 'views/neck.php';   ?>
 <h1>Changes to Dungeon Crawl Chili</h1>
-<p>As of July 3rd at 8 am</p>
+<p>As of July 16th</p>
 
 <h2>Implemented already:</h2>
 <ul><li>The species Silent Specter was added from BCadren crawl;</li>
     <li>D:1 is mostly a forested open floor layout;</li>
     <li>D:2 is always the same static floor (for now until someone makes a few new full floor vaults);</li>
     <li>Jessica right now always spawns in the small castle on the D:2 static floor layout, and drops a book 50% of time;</li>
+    <li>Medusa, a naga Unique with the Petrify spell, will always show up on D:2. The intent will be to let her be on D:2 until the player has enough experience...sort of like an early Sygmund.</li>
     <li>D:3 has 3 vaults of castles. this still needs testing since I keep getting the same castle when testing on DCF;</li>
-    <li>There's a new late game Unique called Regal (an octopode with a cloak) that was never added to DCSS over a year ago written by our own MrSquiggles.</li>
+    <li>There are new late game Uniques:
+        <li>Regal, an octopode with a cape, that was never added to DCSS over a year ago written by our own MrSquiggles.</li>
+        <li>Octavia the Heretic, an Octopode of Gozag</li>
+    </li>
 </ul>
 
 <h2>Already planned:</h2>
 <ul><li>D:4 will be @bcadren 's Sewer level;</li>
-    <li>Medusa, a naga Unique with the Petrify spell, will always show up on D:2. The intent will be to let her be on D:2 until the player has enough experience...sort of like an early Sygmund.</li>
-    <li>Shorten the number of floors for each branch (including Dungeon) by 1 floor (except for Orcish Mines)</li>
-    <li>To compensate for the loss of XP from 1 less floor in most branches and Dungeon, we'll make Uniques much more frequent to spawn and possibly worth a bit more XL.</li>
-    <li>Make Jessica not always give Blink as a Spell, so that only 25% of times you get the change of getting Blink on kill...as opposed to 50% of the time</li>
+    <li>Shorten the number of floors for each branch (including Dungeon) by 1 floor (except for Orcish Mines and th Elven Halls)</li>
+    <li>To compensate for the loss of XP from 1 less floor in most branches and Dungeon, we'll make Uniques much more frequent to spawn and possibly worth a bit more XP.</li>
+    <li>Make Jessica not always give Blink as a Spell, so that only 25% of times you get the change of getting Blink on kill...as opposed to 50% of the time currently.</li>
 </ul>
 
 <h2>Suggestions to be considered:</h2>
