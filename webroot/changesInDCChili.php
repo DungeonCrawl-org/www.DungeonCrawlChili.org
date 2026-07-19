dca5fa54dc18b8630bc47bbabed8634f61bc825f
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-19 08:06:57 -0400

    Revise changes for Dungeon Crawl Chili
    Updated the changes in Dungeon Crawl Chili to reflect new features and modifications, including unique spawn rates and layout changes.

--- webroot/changesInDCChili.php
+++ webroot/changesInDCChili.php
@@ -1,28 +1,38 @@
 <?php require 'views/header.php'; ?> 
 <?php require 'views/neck.php';   ?>
 <h1>Changes to Dungeon Crawl Chili</h1>
-<p>As of July 16th</p>
+<p>As of July 19th</p>
 
 <h2>Implemented already:</h2>
 <ul><li>The species Silent Specter was added from BCadren crawl;</li>
-    <li>D:1 is mostly a forested open floor layout;</li>
+    <li>D:1 is mostly a forested open floor layout and has Jessica almost guaranteed;</li>
     <li>D:2 is always the same static floor (for now until someone makes a few new full floor vaults);</li>
-    <li>D:3 has 3 vaults of castles. This still needs testing since I keep getting the same castle when testing on DCF.</li>
+    <li>D:3 has 4 vaults of castles.</li>
+    <li>D:4 is BCadren's sewer (partialy ported).</li>
 </ul>
 <h4>New or modified Uniques:</h4>
-<ul><li>Jessica right now always spawns in the small castle on the D:2 static floor layout, and drops a book 50% of time;</li>
-    <li>Medusa, a naga with the Petrify spell, will always show up on D:2 close to the granite statues. The intent will be to let her be on D:2 until the player has enough experience...sort of like an early Sygmund.</li>
-    <li>Regal, an octopode with a cape, that was never added to DCSS over a year ago written by our own MrSquiggles.</li>
-    <li>Octavia the Heretic, an Octopode of Gozag</li>
+<ul><li>A higher density of Uniques has been implemented in the Dungeon;</li>
+    <li>Jessica drops a book 50% of time;</li>
+    <li>Medusa, a naga with the Petrify spell, will always show up on D:2 close to the granite statues. 
+        The intent will be to let her be on D:2 until the player has enough experience (XP)...sort of like an early Sygmund.
+        Medusa on her own isn't dangerous but when she dies the statues change into adders and one water moccasin emerges from the fountain</li>
+    <li>Regal, an octopode with a cape, that was never added to DCSS over a year ago written by our own MrSquiggles.
+        Regal shows up somewhere in Vaults, Depths, Crypt and Zot.</li>
+    <li>Octavia the Heretic, an Octopode of Gozag can show up in Depths.</li>
 </ul>
 
 <h2>Already planned:</h2>
-<ul><li>D:4 will be BCadren's Sewer level;</li>
-    <li>Shorten the number of floors for each branch (including Dungeon) by 1 floor (except for Orcish Mines and th Elven Halls)</li>
+<ul><li>Shorten the number of floors for each branch (including Dungeon) by 1 floor (except for Orcish Mines and th Elven Halls)</li>
     <li>To compensate for the loss of XP from 1 less floor in most branches and Dungeon, we'll make Uniques much more frequent to spawn and possibly worth a bit more XP.</li>
     <li>Make Jessica not always give Blink as a Spell, so that only 25% of times you get the change of getting Blink on kill...as opposed to 50% of the time currently.</li>
 </ul>
 
 <h2>Suggestions to be considered:</h2>
-<p>none yet</p>
+<ul><li>kuniqs — Sunday, July 19, 2026 at 4:05 AM<br>
+        It'd be good to lower QB's and athame's mindelay to 14 skill, raise rapier's base dam to 10 or so, and give athame dagger stabbing modifier; 
+        all 3 of them have different niches to fill: qb for aux attacks, athame for spellcasters/stabbers, rapier for small races wanting a plain 
+        damage dealer/fallback for other 2 who didn't luck into their best SB type.</li>
+    <li>kuniqs — Sunday, July 19, 2026 at 5:22 AM<br>
+        also: show xp values for monsters; probably remove hounds from d2 because you can easily go up 4 levels on them alone</li>
+</ul>
 <?php require 'views/footer.php'; ?>
