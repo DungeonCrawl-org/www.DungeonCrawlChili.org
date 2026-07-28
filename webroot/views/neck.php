18849cac3e5ab18ea67e38574c6ca9ee2f820811
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-28 17:20:11 -0400

    Update logo image source in neck.php

--- webroot/views/neck.php
+++ webroot/views/neck.php
@@ -3,7 +3,7 @@
 	<div class="toplinks"><br></div>
     	<div class="page_content">
         	<div class="heading">
-            		<h1><center><a href="/"><img src="/img/title_banner/DungeonCrawlChiliOrg-YourLogoHere.gif" width="256" height="256" 
+            		<h1><center><a href="/"><img src="/img/title_banner/DungeonCrawlChiliOrg.gif" width="256" height="256" 
             										title="Contact RoGGa to have your logo placed here."></a>
 			    </center></h1>
 			<div class="fineprint"><center>Come chat in the <a href="https://discord.gg/gMnE5JFcB7" target="_blank">Dungeon Crawl community discord server
