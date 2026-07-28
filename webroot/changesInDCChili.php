39d8bc1b823bc7efe83beda74f4bd56a6653fefb
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-28 10:31:10 -0400

    Update changes and fix typos in changesInDCChili.php
    Updated the date and time for changes, corrected a typo in the spawn rate description, and added a new unique character. Also modified the silence aura mutation description and adjusted the suggestions section.

--- webroot/changesInDCChili.php
+++ webroot/changesInDCChili.php
@@ -1,7 +1,7 @@
 <?php require 'views/header.php'; ?> 
 <?php require 'views/neck.php';   ?>
 <h1>Changes to Dungeon Crawl Chili</h1>
-<p>As of July 27 @ 8:30am Eastern time</p>
+<p>As of July 28 @ 10:30am Eastern time</p>
 
 <pre>MASSIVE SPOILERS BELOW!</pre>
 
@@ -11,17 +11,17 @@
     <li>D:2 is has 4 different floors and Medusa is garanteed.</li>
     <li>D:3 is BCadren's sewer with Oskar almost guaranteed.</li>
     <li>The number of floors for each branch (including Dungeon) was reduced by 1 floor (except for Orcish Mines, the Elven Halls and Crypt).</li>
-    <li>To compensate for the loss of XP from 1 less floor in most branches and Dungeon, Uniques have a higher spawn rate.</li>
+    <li>To compensate for the loss of XP from 1 less floor in most branches and Dungeon, Uniques have a mcuh higher spawn rate. (to be tested)</li>
 </ul>
 <h4>New or modified Uniques:</h4>
 <ul><li>Jessica drops a book 50% of time with the Blink spell being included 50% of the time.</li>
     <li>Medusa, a naga with the Petrify spell, will always show up on D:2 close to the granite statues.
         Medusa on her own isn't dangerous but when she dies the statues change into adders and one water moccasin emerges from the fountain.</li>
     <li>Oskar the Grump is a new D:3 unique that throws garbage bags at you.</li>
+    <li>Octavia the Heretic, an Octopode of Gozag can show up in Depths.</li>
     <li>Verity the Stone Dragon has the Stone Arrow spell and shows up in the Lair.</li>
     <li>Regal, an octopode with a cape, that was never added to DCSS over a year ago and was written by our own MrSquiggles.
         Regal can show up somewhere in Vaults, Depths, Crypt or Zot.</li>
-    <li>Octavia the Heretic, an Octopode of Gozag can show up in Depths.</li>
 </ul>
 
 <h4>Imported from BCadren Crawl</h4>
@@ -34,6 +34,7 @@
 <ul><li>Lowered QB's and athame's mindelay to 14 skill, raised rapier's base dam to 10, and gave athame the dagger-stabbing modifier.</li>
     <li>The +6 Iskenderun Plasma Blade is a dagger type weapon that deals unresistable damage!</li>
     <li>The XP value is shown in the monster's description.</li>
+    <li>Changes the silence aura mutation to a 3 tier silence halo mutation that the player is not silenced.</li>
 </ul>
 
 <h2>Already planned prior to CC Forks Tournament:</h2>
@@ -43,15 +44,14 @@
 </ul>
 
 <h2>Suggestions to be considered:</h2>
-<ul><li>kuniqs — Remove the silence aura mutation, please.<br>
-        rogga - make into a donut silence aura? ...where the player isn't affected.</li>
-    <li>kuniqs — Friday, July 24, 2026 at 5:37 AM<br>
+<ul><li>kuniqs — Friday, July 24, 2026 at 5:37 AM<br>
         Idea for a new mutation:<br>
         "You have large clown feet"<br>
         "You have clown-like feet"<br>
         "You have clown shoes instead of feet"<br>
         -(--) stealth, 3(4 5) base damage, kick trigger chance aux attack with 5% (10 15) chance to unresistably confuse the target for 1 (2 3) turns.
-        3rd level prohibits footwear, works only without boots like the kick</li>
+        3rd level prohibits footwear, works only without boots like the kick<br>
+        rogga - add the throw pie from killer klowns?</li>
     <li>Draconius — Monday, July 27, 2026 at 9:29 PM: Remove Malevolent Forces please lol</li>
 </ul>
 <?php require 'views/footer.php'; ?>
